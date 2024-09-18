5e6b2d40cd7f688279cc81717e5e7b7892c7a512
bielgirao <bielgirao@hotmail.com>
2024-09-18 18:54:25 -0300

    fix: task type import on task service

--- src/app/services/task-service.service.ts
+++ src/app/services/task-service.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import {Task} from "./task";
+import {Task} from "../types/task";
 import {Environment} from "../../environments/environment";
 
 @Injectable({
